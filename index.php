ed45fb5aed6508f6663e1de078db60aeb0d65999
princesscheryl <donkor.princesscheryl@gmail.com>
2025-10-26 03:58:34 +0000

    header

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require_once 'settings/core.php';
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>Home - E-Commerce Platform</title>
+    <title>Home - Shopify</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
     <link href="css/index.css" rel="stylesheet">
